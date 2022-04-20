8635b2a282e1d2616bf3bcb7adab70fcfcad6f67
Bui Hoang Long <hoanglongktbv1@gmail.com>
2022-04-20 18:49:07 +0700

    Bootstrap2

---
+++ js/main.js
@@ -0,0 +1,36 @@
+// navbar
+$(window).scroll(function(){
+    let oTop = $(".section-2").offset().top - windowinnerHeight;
+    if($(window).scrollTop() > oTop) {
+        $('.navbar').addClass('sticky');
+    }
+    else{
+        $('.navbar').moveClass('sticky');
+    }
+})
+
+// gioi thieu sach
+let nCount = selector => {
+    $(selector).each(function(){
+        $(this).animate({
+            Counter: $(this).text()
+        },{
+            duration: 4000,
+            easing: "swing",
+
+            step: function(value){
+                $(this).text(Math.ceil(value))
+            }
+        })      
+    })
+}
+let position = 0;
+$(window).scroll(function(){
+    let oTop = $('.numbers').offset().top - window.innerHeight
+    if(position == 0 && $(window).scrollTop() >= oTop) {
+        position++;
+        nCount(".rect > h1");
+    }
+})
+
+
